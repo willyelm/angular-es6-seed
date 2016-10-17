ffb9447530ad34025264d7730d16209140de5227
Matthias Kern <mattkern42@yahoo.de>
2016-10-17 22:19:28 +0100

    fix copy paste errors in test for view2

--- app/view2/view2-test.js
+++ app/view2/view2-test.js
@@ -3,9 +3,9 @@ import {module, inject} from 'mocks'
 import {View2Controller} from './view2-controller'
 import view2 from './view2'
 
-describe('myApp.view1 module', () => {
+describe('myApp.view2 module', () => {
   beforeEach(module(view2.name))
-  describe('view1 controller', () => {
+  describe('view2 controller', () => {
     it('should ....', inject(($controller) => {
       var view2Ctrl = $controller(View2Controller)
       expect(view2Ctrl).toBeDefined()
